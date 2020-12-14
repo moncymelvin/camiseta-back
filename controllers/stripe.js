217b4ba6c08264a8f93fa13007195525e4e5aad3
moncymelvin <moncymelvin@gmail.com>
2020-12-15 00:33:14 +0400

    first commit

---
+++ controllers/stripe.js
@@ -0,0 +1,54 @@
+const User = require("../models/user");
+const Cart = require("../models/cart");
+const Product = require("../models/product");
+const Coupon = require("../models/coupon");
+const coupon = require("../models/coupon");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+exports.createPaymentIntent = async (req, res) => {
+  // console.log(req.body);
+  const { couponApplied } = req.body;
+
+  // later apply coupon
+  // later calculate price
+
+  // 1 find user
+  const user = await User.findOne({ email: req.user.email }).exec();
+  // 2 get user cart total
+  const { cartTotal, totalAfterDiscount } = await Cart.findOne({
+    orderdBy: user._id,
+  }).exec();
+  // console.log("CART TOTAL", cartTotal, "AFTER DIS%", totalAfterDiscount);
+
+  let finalAmount = 0;
+
+  if (couponApplied && totalAfterDiscount) {
+    finalAmount = totalAfterDiscount * 100;
+  } else {
+    finalAmount = cartTotal * 100;
+  }
+
+  // create payment intent with order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: finalAmount,
+    currency: "usd",
+    description:"Software development services",
+    shipping: {
+      name: 'Jenny Rosen',
+      address: {
+        line1: '510 Townsend St',
+        postal_code: '98140',
+        city: 'San Francisco',
+        state: 'CA',
+        country: 'US',
+      },
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+    cartTotal,
+    totalAfterDiscount,
+    payable: finalAmount,
+  });
+};
---
+++ firebase/index.js
@@ -0,0 +1,11 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ecommerce-mern-970f3.firebaseio.com"
+});
+
+
+module.exports = admin;
